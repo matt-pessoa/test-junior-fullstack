b3f05f6b2c8a123ad2b24a419822063f5ab8490d
matt-pessoa <omatheuspessoa@gmail.com>
2023-09-30 10:21:43 -0300

    [setup] typescript

---
+++ server/src/routes/files.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const router = Router();
+// const { fileController } = require('../controllers');
+
+// router.post('/', fileController.postFile);
+
+export default router;
