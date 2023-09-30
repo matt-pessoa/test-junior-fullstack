0217293bfc995f63ffd6b3c27f35d8d904217b88
matt-pessoa <omatheuspessoa@gmail.com>
2023-09-30 17:32:33 -0300

    [feat] initialize db

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,19 @@
 import express from 'express';
+import sequelize from './utils/database';
 
-const usersRoute = require('./routes/users.routes');
+// const usersRoute = require('./routes/users.routes');
 // const filesRoute = require('./routes/files.routes');
 const defaultRoute = '/api';
+
+sequelize.sync().then(() => {
+  console.log('DB is ready');
+});
 
 const app = express();
 
 app.use(express.json());
 
-app.use(`${defaultRoute}/users`, usersRoute);
+// app.use(`${defaultRoute}/users`, usersRoute);
 // app.use(`${defaultRoute}/files`, filesRoute);
 
 export default app;
--- server/src/utils/database.ts
+++ server/src/utils/database.ts
@@ -1,21 +1,8 @@
-import sqlite3 from 'sqlite3';
+import { Sequelize } from 'sequelize';
 
-export const openConnection = () => {
-  let db = new sqlite3.Database('./database.db');
-  return db;
-};
+const sequelize = new Sequelize('database', 'user', 'pass', {
+  dialect: 'sqlite',
+  host: './dev.sqlite',
+});
 
-export const dbQuery = (query: string, params?: any[]) => {
-  let db = openConnection();
-  return new Promise<any[]>((resolve, reject) => {
-    db.all(query, params, (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  }).finally(() => {
-    db.close();
-  });
-};
+export default sequelize;
