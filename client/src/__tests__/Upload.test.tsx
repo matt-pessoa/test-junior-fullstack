fe19e53d31d2d57ab07084302e434e2eee38263a
matt-pessoa <omatheuspessoa@gmail.com>
2023-10-01 06:40:04 -0300

    [test] file label

--- client/src/__tests__/Upload.test.tsx
+++ client/src/__tests__/Upload.test.tsx
@@ -20,4 +20,10 @@ describe('Upload component', () => {
       `Say goodbye to the hassle of manual data entry. Our CSV import feature allows you to effortlessly upload your data with just a few clicks. Simply import your CSV file, and we'll take care of the rest.`
     );
   });
+
+  it('displays the "Choose File" label', () => {
+    render(<Upload />);
+    const chooseFileLabel = screen.getByTestId('u-3');
+    expect(chooseFileLabel.textContent).toBe('Choose File');
+  });
 });
