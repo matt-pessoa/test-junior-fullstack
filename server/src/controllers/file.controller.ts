dfbf1d19903aa2a0b2476c49028435a1da79578d
matt-pessoa <omatheuspessoa@gmail.com>
2023-09-30 23:40:04 -0300

    [setup] read CSV

--- server/src/controllers/file.controller.ts
+++ server/src/controllers/file.controller.ts
@@ -1,13 +1,31 @@
 import { Request, Response } from 'express';
+import { Readable } from 'stream';
+import readLine from 'readline';
 import FilesService from '../services/file.service';
 
 export default class FilesController {
   constructor(private fileService = new FilesService()) {}
 
   postData = async (req: Request, res: Response) => {
+    const { file } = req;
+    if (file) {
+      const { buffer } = file;
+
+      const readableFile = new Readable();
+      readableFile.push(buffer);
+      readableFile.push(null);
+
+      const dataLine = readLine.createInterface({
+        input: readableFile,
+      });
+
+      for await (let line of dataLine) {
+        console.log(line);
+      }
+
+      return res.send();
+    }
     // const response = await this.fileService.postData(req.body);
-    console.log(req.file);
     // return res.status(201).json(response);
-    res.send();
   };
 }
