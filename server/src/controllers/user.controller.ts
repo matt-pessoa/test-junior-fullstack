1fe2a7387300cd0fb88f25941435d8511ea165b0
matt-pessoa <omatheuspessoa@gmail.com>
2023-10-01 02:03:59 -0300

    [feat] get partial match

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -5,7 +5,20 @@ export default class UsersController {
   constructor(private userService = new UsersService()) {}
 
   getData = async (req: Request, res: Response) => {
-    const response = await this.userService.getData();
+    const { query } = req;
+
+    if (!query.q) {
+      const response = await this.userService.getData();
+      return res.status(200).json(response);
+    } else {
+      this.getQueryData(req, res);
+    }
+  };
+
+  getQueryData = async (req: Request, res: Response) => {
+    const query = req.query.q;
+
+    const response = await this.userService.getQueryData(query);
     return res.status(200).json(response);
   };
 }
--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import File from '../models/file.model';
 
 export default class UsersService {
@@ -5,6 +6,37 @@ export default class UsersService {
 
   public async getData() {
     const data = await File.findAll();
+
+    return data;
+  }
+
+  public async getQueryData(query: any) {
+    const data = await File.findAll({
+      where: {
+        [Op.or]: [
+          {
+            name: {
+              [Op.like]: `%${query}%`,
+            },
+          },
+          {
+            city: {
+              [Op.like]: `%${query}%`,
+            },
+          },
+          {
+            country: {
+              [Op.like]: `%${query}%`,
+            },
+          },
+          {
+            favorite_sport: {
+              [Op.like]: `%${query}%`,
+            },
+          },
+        ],
+      },
+    });
 
     return data;
   }
